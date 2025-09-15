91f12bef240beb67115c4cf8667a28f6006b282a
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-09-15 12:22:00 -0600

    fix: conditional class not generated in talwind bundle

--- src/components/common/page-section/index.tsx
+++ src/components/common/page-section/index.tsx
@@ -1,7 +1,8 @@
+import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 
 export default function PageSection({ children, asPage }: PageSectionProps) {
-  return <section className={`mt-${asPage ? 10 : 20}`}>{children}</section>;
+  return <section className={cn(`mt-${asPage ? 10 : 20}`)}>{children}</section>;
 }
 
 type PageSectionProps = PropsWithChildren<{
