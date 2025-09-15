0314da2df773313385f01b091100e6dbb13e5daf
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-09-15 00:53:48 -0600

    responsive: experience

--- src/components/common/timeline/item.tsx
+++ src/components/common/timeline/item.tsx
@@ -3,6 +3,9 @@ import moment from "moment";
 import TechItem from "../tech-item";
 import type { ExperienceItem } from "@/lib/types";
 
+import "moment/locale/es";
+moment.locale("es");
+
 export default function TimelineItem(props: TimelineItemProps) {
   const { dates, jobTitle, company, location, current, children, techs } =
     props;
@@ -15,10 +18,10 @@ export default function TimelineItem(props: TimelineItemProps) {
 
   const ToDateContainer = toIsToday || current ? "div" : "time";
   return (
-    <li className="ms-4 [&:not(:last-child)]:mb-10">
+    <li className="ms-4 not-[:last-child]:mb-10">
       <div className="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-      <div className="mb-1 flex justify-between text-sm leading-none font-normal text-gray-400 capitalize dark:text-gray-500">
-        <div className="flex gap-4">
+      <div className="mb-1 grid grid-cols-[60%_auto] text-sm leading-none font-normal text-gray-400 capitalize dark:text-gray-500">
+        <div className="flex gap-2 sm:gap-4">
           <time dateTime={fromMoment.format("YYYY-MM-DD")}>
             {fromMoment.format("MMMM YYYY")}
           </time>
@@ -29,15 +32,15 @@ export default function TimelineItem(props: TimelineItemProps) {
             {toIsToday ? "Actualmente" : toMoment.format("MMMM YYYY")}
           </ToDateContainer>
         </div>
-        <span>{location}</span>
+        <span className="text-end">{location}</span>
       </div>
       <h3 className="flex justify-between text-lg font-semibold text-(--chart-4)">
-        <span>{jobTitle}</span> <span>{company}</span>
+        <span>{jobTitle}</span> <span className="text-end">{company}</span>
       </h3>
       <div className="mt-4 mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
         {children}
       </div>
-      <div className="flex items-center gap-4 text-xl">
+      <div className="flex items-center gap-2 text-xl sm:gap-4">
         <span className="text-[1rem]">Tecnologías usadas:</span>
         {techs.map((tech, i) => (
           <TechItem key={i} tech={tech} />
