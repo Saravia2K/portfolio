d5965f8648d3844c67b4b2fa6c816868ed702f15
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-09-15 16:02:29 -0600

    initial version for JSON-LD

---
+++ src/app/(portfolio)/components/JSONLD.tsx
@@ -0,0 +1,31 @@
+import Script from "next/script";
+
+import EXPERIENCE from "@/assets/json/experience";
+
+export default function JSONLD() {
+  const LdJSON = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: "Diego Saravia",
+    hasOccupation: EXPERIENCE.map((e) => ({
+      "@type": "Role",
+      roleName: e.jobTitle,
+      startDate: e.dates.from,
+      endDate: e.current ? undefined : e.dates.to,
+      memberOf: {
+        "@type": "Organization",
+        name: e.company,
+      },
+    })),
+  };
+
+  return (
+    <Script
+      type="application/ld+json"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(LdJSON).replace(/</g, "\\u003c"),
+      }}
+    />
+  );
+}
--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,9 +1,11 @@
 import { type PropsWithChildren } from "react";
 import ProfileCard from "./components/ProfileCard";
+import JSONLD from "./components/JSONLD";
 
 export default function PortfolioLayout({ children }: PropsWithChildren) {
   return (
     <section className="z-10 min-h-screen w-[90%] text-wrap text-white sm:w-[80%] xl:grid xl:w-[70%] xl:grid-cols-[auto_1fr] xl:gap-20">
+      <JSONLD />
       <aside className="m-auto mb-10 w-fit xl:sticky xl:top-40 xl:m-0 xl:h-fit xl:self-start">
         <ProfileCard />
       </aside>
