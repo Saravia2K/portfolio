63d7bc60fc6058806963ccf89f1a24fb402bbb2e
Diego Saravia <diego.saraviazaldana@gmail.com>
2026-03-13 22:23:57 -0600

    feat: añadiendo funciones de utilidad

---
+++ src/lib/server-utils.ts
@@ -0,0 +1,4 @@
+import { createServerOnlyFn } from '@tanstack/react-start'
+
+// Get environment variables on server side
+export const getEnv = createServerOnlyFn((key: string) => process.env[key])
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { createIsomorphicFn } from '@tanstack/react-start'
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const consoleLog = createIsomorphicFn()
+  .server((msg: string) => console.log(`[SERVER]: ${msg}`))
+  .client((msg: string) => console.log(`[CLIENT]: ${msg}`))
