1bca24a5977d3d4e3351f61b63a941b33cf225f5
Diego Saravia <diego.saraviazaldana@gmail.com>
2026-03-16 15:32:46 -0600

    fix: removiendo fix para urlpattern global

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-import { URLPattern as URLPatternPolyfill } from "urlpattern-polyfill";
-if (typeof (globalThis as any).URLPattern === "undefined") {
-  (globalThis as any).URLPattern = URLPatternPolyfill;
-}
 import { paraglideMiddleware } from "./paraglide/server.js";
 import handler from "@tanstack/react-start/server-entry";
 export default {
